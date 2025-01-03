mollyim/mollyim-android

Update brand logo and colors

app/src/main/java/org/thoughtcrime/securesms/jobs/CreateReleaseChannelJob.kt
@@ -76,7 +76,7 @@ class CreateReleaseChannelJob private constructor(parameters: Parameters) : Base
     AvatarRenderer.renderAvatar(
       context,
       Avatar.Resource(
-        R.drawable.ic_molly_logo,
+        R.drawable.ic_molly_logo_large,
         Avatars.ColorPair(ContextCompat.getColor(context, R.color.notification_background_ultramarine), ContextCompat.getColor(context, R.color.core_white), "")
       ),
       onAvatarRendered = { media ->

Rukumango/Signal-Mod

app/src/main/java/org/thoughtcrime/securesms/jobs/CreateReleaseChannelJob.kt
@@ -76,7 +76,7 @@ class CreateReleaseChannelJob private constructor(parameters: Parameters) : Base
     AvatarRenderer.renderAvatar(
       context,
       Avatar.Resource(
-        R.drawable.ic_molly_logo,
+        R.drawable.logo_round_filled,
         Avatars.ColorPair(ContextCompat.getColor(context, R.color.notification_background_ultramarine), ContextCompat.getColor(context, R.color.core_white), "")
       ),
       onAvatarRendered = { media ->